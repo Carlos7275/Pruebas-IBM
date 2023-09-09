a6ced6225e87f80b0d7a10ccc39d336a4c9bd9ad
Carlos Fernando Sandoval Lizárraga <Carlos_72_75@hotmail.com>
2023-09-09 11:48:57 -0700

    1

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// require expressjs
-const express = require("express")
-const app = express()
-// define port 8080
-PORT = 8080
-app.use(express.json())
-// use router to bundle all routes to /
-const router = express.Router()
-app.use("/", router)
-// get on root route
-router.get("/", (req,res) => {
-	res.send("hello world!!!")
-})
+const express = require('express');
+const helmet = require('helmet');
+const pino = require('pino')();
+const PORT = process.env.PORT || 3000;
 
-// start server
+const app = express();
+
+app.use(helmet());
+
+app.get('/', (req, res) => {
+   res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
-	console.log("Server is up and running!!")
-})
+   pino.info(`Server listening on port ${PORT}`);
+});
