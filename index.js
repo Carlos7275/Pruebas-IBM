e5a44442e7d3febd8afe2e62687358bfeabd1d49
Carlos Sandoval <carlos_72_75@hotmail.com>
2023-09-09 12:30:45 -0700

    111

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const helmet = require('helmet');
 const pino = require('pino')();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(helmet());
 
 app.get('/', (req, res) => {
    res.send('Hello, World!');
