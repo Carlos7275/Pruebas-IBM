ceb3d5c388f3d351228cf0bd093a6e50b5852207
Carlos Sandoval <carlos_72_75@hotmail.com>
2023-09-09 12:44:39 -0700

    .

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-   res.send('Hello, World!');
+   res.send('Hola Mundo!');
 });
+app.get("Saludar/:name", (req, res) => {
+   res.send(`Hola ${req.param("name")}`)
+})
+
+app.get("/Sumar/:num1/:num2", (req, res) => {
+   var total=Number(req.param("num1"))+Number(req.param("num2"));
+   res.json({"Message":"El resultado de la suma es : "+total})
+})
 
 app.listen(PORT, () => {
    pino.info(`Server listening on port ${PORT}`);
