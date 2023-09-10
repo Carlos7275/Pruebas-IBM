3836bc3eaf44d2442baf0a1eeedf1d042e57c475
Carlos Sandoval <carlos_72_75@hotmail.com>
2023-09-10 10:51:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,42 +4,47 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-
 app.get('/', (req, res) => {
    res.send('Hola Mundo!');
 });
-app.get("Saludar/:name", (req, res) => {
+app.get("/Saludar/:name", (req, res) => {
    res.send(`Hola ${req.params.name}`)
 })
 
 app.get("/Calculadora/:num1/:operacion/:num2", (req, res) => {
    var operacion = String(req.params.operacion);
+   const { num1, num2 } = req.params;
+
    total = 0.0;
    switch (operacion) {
       case "+":
-         total = Number(req.params.num1) + Number(req.params.num2);
+         total = Number(num1) + Number(num2);
          break;
       case "-":
-         total = Number(req.params.num1) - Number(req.params.num2);
+         total = Number(num1) - Number(num2);
          break;
       case "*":
-         total = Number(req.params.num1) * Number(req.params.num2);
+         total = Number(num1) * Number(num2);
          break;
-      case "/":
-         total = Number(req.params.num1) / Number(req.params.num2);
+      case "div":
+         if (num2 != 0)
+            total = Number(num1) / Number(num2);
+         else
+            res.json({ "Message": "No se puede dividir entre 0" })
          break;
       default:
          res.json({ "Message": "Operación Invalida" });
          break;
    }
 
-   res.json({ 
+   res.json({
+      "Number1": num1,
+      "Number2": num2,
       "Operation": operacion,
-      "Result": total, 
-      }
+      "Result": total,
+   }
    )
 })
-
 
 app.listen(PORT, () => {
    pino.info(`Server en Escucha en el Puerto: ${PORT}`);
