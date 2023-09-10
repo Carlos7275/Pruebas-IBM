4b7158334034317f68fec31bb2e74cd3ed32cdd6
Carlos Sandoval <carlos_72_75@hotmail.com>
2023-09-10 10:09:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,38 @@ app.get('/', (req, res) => {
    res.send('Hola Mundo!');
 });
 app.get("Saludar/:name", (req, res) => {
-   res.send(`Hola ${req.param("name")}`)
+   res.send(`Hola ${req.params.name}`)
 })
 
-app.get("/Sumar/:num1/:num2", (req, res) => {
-   var total=Number(req.param("num1"))+Number(req.param("num2"));
-   res.json({"Message":"El resultado de la suma es : "+total})
+app.get("/Calculadora/:num1/:operacion/:num2", (req, res) => {
+   var operacion = String(req.params.operacion);
+   total = 0.0;
+   switch (operacion) {
+      case "+":
+         total = Number(req.params.num1) + Number(req.params.num2);
+         break;
+      case "-":
+         total = Number(req.params.num1) - Number(req.params.num2);
+         break;
+      case "*":
+         total = Number(req.params.num1) * Number(req.params.num2);
+         break;
+      case "/":
+         total = Number(req.params.num1) / Number(req.params.num2);
+         break;
+      default:
+         res.json({ "Message": "Operación Invalida" });
+         break;
+   }
+
+   res.json({ 
+      "Operation": operacion,
+      "Result": total, 
+      }
+   )
 })
+
 
 app.listen(PORT, () => {
-   pino.info(`Server listening on port ${PORT}`);
+   pino.info(`Server en Escucha en el Puerto: ${PORT}`);
 });
